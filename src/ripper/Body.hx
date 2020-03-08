2b2f614a02f8a5ac7fbe8080e2213cbe66a17a27
FAL <falworks.contact@gmail.com>
2020-03-09 07:26:09 +0900

    add comments

--- src/ripper/Body.hx
+++ src/ripper/Body.hx
@@ -1,4 +1,12 @@
 package ripper;
 
+/**
+	Interface for indicating that it is not fully implemented by itself and
+	needs other classes that are marked with `Spirit` interface.
+
+	Set `@:ripper.spirits(...)` metadata for each class that implements `Body`.
+	Any `Spirit` class can be specified here so that the implementation will be
+	copied from the `Spirit` to the `Body`.
+**/
 @:autoBuild(ripper.macro.BodyMacro.build())
 interface Body {}
--- src/ripper/Spirit.hx
+++ src/ripper/Spirit.hx
@@ -1,4 +1,8 @@
 package ripper;
 
+/**
+	Interface for indicating that it is a reusable component for other class
+	that are marekd with `Body` interface.
+**/
 @:autoBuild(ripper.macro.SpiritMacro.register())
 interface Spirit {}
