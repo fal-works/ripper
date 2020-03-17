4247943250e615ae14491fa8db90932b4d6b1828
FAL <falworks.contact@gmail.com>
2020-03-17 09:18:42 +0900

    code format

--- src/ripper/macro/utility/ClassTypeExtension.hx
+++ src/ripper/macro/utility/ClassTypeExtension.hx
@@ -7,7 +7,10 @@ class ClassTypeExtension {
 	/**
 		@return `true` if `this` implements interface with name `interfaceName`.
 	**/
-	public static function implementsInterface(_this: ClassType, interfaceName: String): Bool {
+	public static function implementsInterface(
+		_this: ClassType,
+		interfaceName: String
+	): Bool {
 		final interfaces = _this.interfaces;
 		for (i in 0...interfaces.length)
 			if (interfaces[i].t.toString() == interfaceName) return true;
